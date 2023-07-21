135655f7babac8ac23d9ebe94d1aff871a4e141f
alfarrabi26 <61672216+alfarrabi26@users.noreply.github.com>
2023-07-22 01:39:08 +0700

    Update popup.js

--- js/controller/popup.js
+++ js/controller/popup.js
@@ -3,6 +3,7 @@ import {toStringHDMS} from 'https://cdn.skypack.dev/ol/coordinate.js';
 import {overlay,map,popupinfo,idmarker} from '../config/peta.js';
 import {clickpopup} from '../template/popup.js';
 import {insertMarker,deleteMarker} from './marker.js';
+
 export function container(id){
     return document.getElementById(id);
 }
