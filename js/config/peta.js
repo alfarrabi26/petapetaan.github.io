c1447c0bd7630b07a2a1309a9e0605143e1c00a5
alfarrabi26 <61672216+alfarrabi26@users.noreply.github.com>
2023-07-22 01:41:42 +0700

    Update peta.js

--- js/config/peta.js
+++ js/config/peta.js
@@ -5,8 +5,62 @@ import XYZ from 'https://cdn.skypack.dev/ol/source/XYZ.js';
 import OSM from 'https://cdn.skypack.dev/ol/source/OSM.js';
 import {fromLonLat} from 'https://cdn.skypack.dev/ol/proj.js';
 import Overlay from 'https://cdn.skypack.dev/ol/Overlay.js';
-import {container} from 'https://jscroot.github.io/element/croot.js';
 
+export function container(id){
+    return document.getElementById(id);
+}
+
+export function onClick(id,actionfunctionname){
+    document.getElementById(id).onclick = actionfunctionname;
+}
+
+export function onChange(id,actionfunctionname){
+    document.getElementById(id).onchange = function() {actionfunctionname()};
+}
+
+export function textFocus(id){
+    document.getElementById(id).focus();
+}
+
+export function textBlur(id){
+    document.getElementById(id).blur();
+}
+
+export function getValue(id){
+    return document.getElementById(id).value;
+}
+
+export function setValue(id,valuecontent){
+    return document.getElementById(id).value=valuecontent;
+}
+
+export function setInner(id,content){
+    document.getElementById(id).innerHTML = content;
+}
+
+export function addInner(id,content){
+    document.getElementById(id).innerHTML += content;
+}
+
+export function addChild(id,tag,classvalue,content){
+    let el = document.createElement(tag);
+    let classArray = classvalue.split(" ");
+    classArray.forEach(setClassValue.bind(null,el));
+    el.innerHTML = content;
+    document.getElementById(id).appendChild(el);
+}
+
+function setClassValue(el,classvalue){
+    el.classList.add(classvalue.trim());
+}
+
+export function show(id){
+    document.getElementById(id).style.display = 'block';
+}
+
+export function hide(id){
+    document.getElementById(id).style.display = 'none';
+}
 
 const place = [107.6175, -6.9039];
 
