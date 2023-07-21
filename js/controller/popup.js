1979e4c53b0effb9853e7b3d6701e85a7027559d
alfarrabi26 <61672216+alfarrabi26@users.noreply.github.com>
2023-07-22 01:37:32 +0700

    Update popup.js

--- js/controller/popup.js
+++ js/controller/popup.js
@@ -4,7 +4,57 @@ import {overlay,map,popupinfo,idmarker} from '../config/peta.js';
 import {clickpopup} from '../template/popup.js';
 import {insertMarker,deleteMarker} from './marker.js';
 import {setInner,textBlur,onClick, getValue,setValue} from 'https://jscroot.github.io/element/croot.js';
-import { postWithToken } from "https://jscroot.github.io/api/croot.js";
+export function postWithToken(target_url,tokenkey,tokenvalue,datajson,responseFunction){
+    var myHeaders = new Headers();
+    myHeaders.append(tokenkey, tokenvalue);
+    myHeaders.append("Content-Type", "application/json");
+
+    var raw = JSON.stringify(datajson);
+
+    var requestOptions = {
+    method: 'POST',
+    headers: myHeaders,
+    body: raw,
+    redirect: 'follow'
+    };
+
+    fetch(target_url, requestOptions)
+    .then(response => response.text())
+    .then(result => responseFunction(JSON.parse(result)))
+    .catch(error => console.log('error', error));
+}
+
+export function postWithBearer(target_url,token,datajson,responseFunction){
+    var myHeaders = new Headers();
+    myHeaders.append("Authorization", "Bearer "+token);
+    myHeaders.append("Content-Type", "application/json");
+
+    var raw = JSON.stringify(datajson);
+
+    var requestOptions = {
+    method: 'POST',
+    headers: myHeaders,
+    body: raw,
+    redirect: 'follow'
+    };
+
+    fetch(target_url, requestOptions)
+    .then(response => response.text())
+    .then(result => responseFunction(JSON.parse(result)))
+    .catch(error => console.log('error', error));
+}
+
+export function get(target_url,responseFunction){
+    var requestOptions = {
+    method: 'GET',
+    redirect: 'follow'
+    };
+
+    fetch(target_url, requestOptions)
+    .then(response => response.text())
+    .then(result => responseFunction(JSON.parse(result)))
+    .catch(error => console.log('error', error));
+}
 
 
 export function onClosePopupClick() {
